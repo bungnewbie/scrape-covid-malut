bbb96580ecd13ca65553333288a354657ac3993c
Rizkhal Lamaau <lamaaurizkhal@gmail.com>
2020-03-26 02:28:03 +0700

    tes hello world message

--- src/Telegram/GetUpdate.php
+++ src/Telegram/GetUpdate.php
@@ -10,7 +10,7 @@ namespace Bot\Telegram;
 
 use Bot\Telegram\TelegramBot;
 
-final class GetUpdate
+class GetUpdate
 {
     private $token;
 
@@ -22,39 +22,10 @@ final class GetUpdate
     public function run()
     {
         $telegram = new TelegramBot($this->token);
-        // Get all the new updates and set the new correct update_id
-        $req = $telegram->getUpdates();
 
-        for ($i = 0; $i < $telegram->UpdateCount(); $i++) {
-            // You NEED to call serveUpdate before accessing the values of message in Telegram Class
-            $telegram->serveUpdate($i);
-            $text = $telegram->Text();
-            $chat_id = $telegram->ChatID();
-
-            if ($text == '/start') {
-                $reply = 'Working';
-                $content = ['chat_id' => $chat_id, 'text' => $reply];
-                $telegram->sendMessage($content);
-            }
-            if ($text == '/test') {
-                if ($telegram->messageFromGroup()) {
-                    $reply = 'Chat Group';
-                } else {
-                    $reply = 'Private Chat';
-                }
-                // Create option for the custom keyboard. Array of array string
-                $option = [['A', 'B'], ['C', 'D']];
-                // Get the keyboard
-                $keyb = $telegram->buildKeyBoard($option);
-                $content = ['chat_id' => $chat_id, 'reply_markup' => $keyb, 'text' => $reply];
-                $telegram->sendMessage($content);
-            }
-            if ($text == '/git') {
-                $reply = 'Check me on GitHub: https://github.com/Eleirbag89/TelegramBotPHP';
-                // Build the reply array
-                $content = ['chat_id' => $chat_id, 'text' => $reply];
-                $telegram->sendMessage($content);
-            }
-        }
+        $text = $telegram->Text();
+        $chat_id = $telegram->ChatID();
+        $content = array('chat_id' => $chat_id, 'text' => 'Hello');
+        $telegram->sendMessage($content);
     }
 }
